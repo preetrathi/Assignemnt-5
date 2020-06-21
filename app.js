ddba382017f8244f0fa04321151089a0a413524e
Preet Kumar <40492888+preetrathi@users.noreply.github.com>
2020-06-21 19:46:24 +0500

    Assignment 6

---
+++ app.js
@@ -0,0 +1,71 @@
+var lineBreak = '<br>'
+
+// Chapter 21-25
+
+// Task 01
+// var first_name = prompt("Enter your first name");
+// var last_name = prompt("Enter your last name");
+// var ful_name = first_name + " " + last_name;
+// document.write("Hello " + ful_name + "<br>");
+
+
+// // Task 02
+// var mobile_model = prompt("Write something about his favourte mobile: ")
+// var string_length = mobile_model.length;
+// document.write(mobile_model)
+// document.write(string_length +"<br>");
+
+// // Task 03
+// var nationality = "Pakistani"
+// document.write("Pakistani index of n is " + nationality.indexOf('n') + "<br>")
+
+// Task 04
+// var word = "Hello World"
+// document.write("String Hello World last index of L : " + word.lastIndexOf('l'))
+
+// Task 05
+// document.write("pakistani".charAt(3));
+
+// Task 06
+// var first_name = prompt("Enter your first name");
+// var last_name = prompt("Enter your last name");
+// document.write(first_name.concat(last_name));
+
+// Task 07
+// var string = "Hyderabad";
+// document.write(string.replace('Hyder', "Islam"));
+
+
+// Task 08
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// document.write(message.replace(/and/g , '&'));
+
+// Task 09
+// var string = '472';
+// var convet = Number(string);
+// document.write("Value: " + string + "<br>");
+// document.write("Type: " + typeof(string) + "<br>");
+// document.write("Value: " + convet + "<br>");
+// document.write("Type: " + typeof(convet) + "<br>");
+
+// Task 10
+// var userInput = prompt("Enter the string");
+// document.write("User Input: " + userInput + '<br>');
+// document.write("Upper case: " + userInput.toUpperCase() );
+
+// Task 11
+// var userInput = prompt("Enter the string");
+// var first_char = userInput.slice(0,1);
+// var other_chars = userInput.slice(1);
+// first_char = first_char.toUpperCase();
+// other_chars = other_chars.toLowerCase();
+// document.write("Userinput: " + userInput);
+// document.write("Title case: " + first_char + " " + other_chars);
+
+// Task 12
+// var num = 35.36;
+// var without = num.toString().replace(".", ""); 
+// document.write("Number: " + num + '<br>');
+// document.write("Result: " + without);
+
+
